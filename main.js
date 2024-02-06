10fc7b8acb14f83db09a52191870f52d2488e7bf
Taylor Davis <v.taylordavis@gmail.com>
2024-02-06 15:53:19 -0600

    set up container & sidebar CSS, set up renderToDom function, will review with team before pushing

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("secret message from taylor: hi :3");
+const renderToDom = (divId, html) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = html;
+};
+
+// const sidebarRender = () => {
+//   const sidebarDomString = `
+// <div label="bio-pic">Testing, testing...</div>
+// <div label="bio-name"></div>
+// <div label="bio-copy"></div>
+// <div label="buttons"></div>
+// <div label="stats"></div>
+// <div label="contacts"></div>
+// <div label="highlights"></div>
+// <div label="orgs"></div>
+// <div label="sponsors></div>
+// `;
+//   renderToDom("#sidebar", sidebarDomString);
+// };
