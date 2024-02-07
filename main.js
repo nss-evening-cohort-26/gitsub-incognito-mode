509bfa46d046d0eb1298e5d1ad0634b852074159
Lucas Noel <lucasnoel1985@gmail.com>
2024-02-06 19:13:24 -0600

    fixing branch

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log ("Hello World");
+const renderToDom = (divId, html) => {
+    const selectedDiv = document.querySelector(divId);
+    selectedDiv.innerHTML = html;
+  };
+  
+  // const sidebarRender = () => {
+  //   const sidebarDomString = `
+  // <div label="bio-pic">Testing, testing...</div>
+  // <div label="bio-name"></div>
+  // <div label="bio-copy"></div>
+  // <div label="buttons"></div>
+  // <div label="stats"></div>
+  // <div label="contacts"></div>
+  // <div label="highlights"></div>
+  // <div label="orgs"></div>
+  // <div label="sponsors></div>
+  // `;
+  //   renderToDom("#sidebar", sidebarDomString);
+  // };
